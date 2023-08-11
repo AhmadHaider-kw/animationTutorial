2ffa50707826b6c2696ca7ef22c1670716f298d8
ahmad haider <ahmad.haider.kw@gmail.com>
2023-08-11 14:57:02 +0300

    Initial Commit

---
+++ animationTut/animationTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationTutApp.swift
+//  animationTut
+//
+//  Created by Ahmad Haider on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct animationTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
